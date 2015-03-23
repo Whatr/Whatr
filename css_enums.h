2e43905a378c05406a71926f05906d61744d145b
Jesbus <jessebusman@gmail.com>
2015-03-23 15:28:34 +0100

    Added some more CSS property values...

--- css_enums.h
+++ css_enums.h
@@ -13,9 +13,150 @@
 #ifndef css_enums_included
 #define css_enums_included yes
 
-enum CSSAlignContent
+enum CSSAlignContent // CSS3
 {
-	
+	STRETCH, // Default
+	CENTER,
+	FLEX_START,
+	FLEX_END,
+	SPACE_BETWEEN,
+	SPACE_AROUND,
+};
+enum CSSAlignItems // CSS3
+{
+	STRETCH, // Default
+	CENTER,
+	FLEX_START,
+	FLEX_END,
+	BASELINE,
+};
+enum CSSAlignSelf // CSS3
+{
+	AUTO, // Default
+	STRETCH,
+	CENTER,
+	FLEX_START,
+	FLEX_END,
+	BASELINE,
+};
+enum CSSAnimationDirection // CSS3
+{
+	NORMAL, // Default
+	REVERSE,
+	ALTERNATE,
+	ALTERNATE_REVERSE,
+};
+enum CSSAnimationFillMode // CSS3
+{
+	NONE, // Default
+	FORWARDS,
+	BACKWARDS,
+	BOTH,
+};
+enum CSSAnimationPlayState // CSS3
+{
+	PAUSED,
+	RUNNING, // Default
+};
+enum CSSBackfaceVisibility // CSS3
+{
+	VISIBLE, // Default
+	HIDDEN,
+};
+enum CSSBackgroundAttachment // CSS1
+{
+	SCROLL, // Default
+	FIXED,
+	LOCAL,
+};
+enum CSSBackgroundClip // CSS3
+{
+	BORDER_BOX, // Default
+	PADDING_BOX,
+	CONTENT_BOX,
+};
+enum CSSBackgroundOrigin // CSS3
+{
+	PADDING_BOX, // Default
+	BORDER_BOX,
+	CONTENT_BOX,
+};
+enum CSSBackgroundRepeat // CSS1
+{
+	REPEAT,
+	REPEAT_X,
+	REPEAT_Y,
+	NO_REPEAT,
+};
+enum CSSBorderStyle // CSS1
+{
+	NONE, // Default
+	HIDDEN,
+	DOTTED,
+	DASHED,
+	SOLID,
+	DOUBLE,
+	GROOVE,
+	RIDGE,
+	INSET,
+	OUTSET,
+};
+enum CSSBorderCollapse // CSS2
+{
+	SEPARATE, // Default
+	COLLAPSE,
+};
+enum CSSBorderImageRepeat // CSS3
+{
+	STRETCH, // Default
+	REPEAT,
+	ROUND,
+	SPACE,
+};
+enum CSSBoxSizing // CSS3
+{
+	CONTENT_BOX, // Default
+	BORDER_BOX,
+};
+enum CSSCaptionSide // CSS2
+{
+	TOP, // Default
+	BOTTOM,
+};
+enum CSSClear // CSS1
+{
+	NONE, // Default
+	LEFT,
+	RIGHT,
+	BOTH,
+};
+enum CSSColumnFill // CSS3
+{
+	BALANCE, // Default
+	AUTO,
+};
+enum CSSColumnRuleStyle // CSS3
+{
+	NONE, // Default
+	HIDDEN,
+	DOTTED,
+	DASHED,
+	SOLID,
+	DOUBLE,
+	GROOVE,
+	RIDGE,
+	INSET,
+	OUTSET,
+};
+enum CSSColumnSpan // CSS3
+{
+	ONE, // Default
+	ALL,
+};
+enum CSSDirection // CSS2
+{
+	LTR, // Default
+	RTL,
 };
 enum CSSDisplay
 {
@@ -42,6 +183,6 @@ enum CSSPosition
 {
 	FIXED,
 	ABSOLUTE,
-	RELATIVE
+	RELATIVE,
 };
 #endif
