b501cf745a834b880d6887e389c804bb1153174e
Jesbus <jessebusman@gmail.com>
2015-06-12 20:13:49 +0200

    see prev

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 # Copyright (C) 2015 Jesse Busman
-# You may not profit from or distribute this program or parts
-# of this program in any way, shape or form without written
+# You may not distribute this program or parts of this
+# program in any way, shape or form without written
 # permission from Jesse Busman (born 17 march 1996).
+# This program is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. This message may not be changed
+# or removed.
 
 
 outputProgramFile="whatr.exe"
