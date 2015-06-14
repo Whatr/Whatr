36c5282d64c60061369802f153b924573aaeee2d
Jesbus <jessebusman@gmail.com>
2015-06-14 12:41:08 +0200

    Added license to string matcher generator

--- string_matcher_generator.php
+++ string_matcher_generator.php
@@ -1,3 +1,15 @@
+<!--
+Copyright (C) 2015 Jesse Busman
+You may not distribute this program or parts of this
+program in any way, shape or form without written
+permission from Jesse Busman (born 17 march 1996).
+This program is distributed in the hope that it will
+be useful, but WITHOUT ANY WARRANTY; without even the
+implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. This message may not be changed
+or removed.
+-->
+
 <style>
 div
 {
